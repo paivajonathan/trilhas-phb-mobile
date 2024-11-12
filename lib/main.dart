91a5fe0465103bcb37b0ef1420da022bcc7c888c
Jonathan Paiva <jonathanapaiva@gmail.com>
2024-11-12 12:17:30 -0300

    fix(main_screen): remove unneeded code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import "package:flutter/material.dart";
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import "package:trilhas_phb/screens/auth_check.dart";
-import "package:trilhas_phb/services/auth.dart";
 
 Future<void> main() async {
   await dotenv.load();
