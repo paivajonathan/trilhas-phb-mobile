1e531e5f8f3fc7dee5aff1c3bd72c81e7c94ba9e
Jonathan Paiva <jonathanapaiva@gmail.com>
2025-02-20 19:19:15 -0300

    feat: update calculate age function and tests to be more aaccurate

--- test/unit/calculate_age_test.dart
+++ test/unit/calculate_age_test.dart
@@ -5,28 +5,38 @@ void main() {
   group('calculateAge Tests', () {
     
     test('age when birthday has already passed this year', () {
-      // Assume today is 2025-02-18
+      final today = DateTime(2025, 02, 18);
       final birthDate = DateTime(1990, 1, 1);  // Birthday passed this year
-      final age = calculateAge(birthDate);
-      expect(age, 35);  // Expected age as of 2025-02-18
+      
+      final age = calculateAge(birthDate, today);
+      
+      expect(age, 35);
     });
 
     test('age when birthday is today', () {
       final today = DateTime.now();
       final birthDate = DateTime(today.year, today.month, today.day);  // Birthday is today
+      
       final age = calculateAge(birthDate);
+      
       expect(age, 0);  // The person should be 0 years old if born today
     });
 
     test('age when birthday hasn’t happened yet this year', () {
+      final today = DateTime(2025, 02, 18);
       final birthDate = DateTime(1990, 12, 31);  // Birthday is later this year
-      final age = calculateAge(birthDate);
+      
+      final age = calculateAge(birthDate, today);
+      
       expect(age, 34);  // Expected age is 34, since birthday is in December
     });
 
     test('age when birthdate is in the future (should return negative or error)', () {
+      final today = DateTime(2025, 02, 18);
       final birthDate = DateTime(2050, 1, 1);  // Future birthdate
-      final age = calculateAge(birthDate);
+      
+      final age = calculateAge(birthDate, today);
+      
       expect(age, lessThan(0));  // The function should return negative or handle future date
     });
   });
